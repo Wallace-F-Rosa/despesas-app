ef147e970a5f6cbde7d07a0599b141363334909f
wallace-f-rosa <wferancini@gmail.com>
2020-09-13 20:03:42 -0300

    Adição de ViewPager e constraints para tela principal

--- app/src/main/java/ferancini/app/despesas/view/MainActivity.java
+++ app/src/main/java/ferancini/app/despesas/view/MainActivity.java
@@ -1,16 +1,29 @@
 package ferancini.app.despesas.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 import ferancini.app.despesas.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabMenu;
+    private TabItem tabDespesas;
+    private TabItem tabOrcamento;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tabMenu = findViewById(R.id.tabMenu);
+        this.tabDespesas = findViewById(R.id.tabDespesas);
+        this.tabOrcamento = findViewById(R.id.tabOrcamento);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
     }
 }
