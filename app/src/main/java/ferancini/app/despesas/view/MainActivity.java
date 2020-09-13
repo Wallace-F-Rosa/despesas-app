1c545d344970e9ff5c7ecac69332c201a12b5471
wallace-f-rosa <wferancini@gmail.com>
2020-09-13 19:51:43 -0300

    Adicionando TabLayout com duas tabs

--- app/src/main/java/ferancini/app/despesas/view/MainActivity.java
+++ app/src/main/java/ferancini/app/despesas/view/MainActivity.java
@@ -1,8 +1,10 @@
-package ferancini.app.despesas;
+package ferancini.app.despesas.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import ferancini.app.despesas.R;
 
 public class MainActivity extends AppCompatActivity {
 
