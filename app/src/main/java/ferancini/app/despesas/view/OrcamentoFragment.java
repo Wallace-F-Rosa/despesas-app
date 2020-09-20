6891f9e9c09c5dcbb4cd2a14cfc7ac4a56bcefc6
wallace-f-rosa <wferancini@gmail.com>
2020-09-20 18:22:21 -0300

    carregando recycleview em OrcamentoFragment

--- app/src/main/java/ferancini/app/despesas/view/OrcamentoFragment.java
+++ app/src/main/java/ferancini/app/despesas/view/OrcamentoFragment.java
@@ -3,12 +3,15 @@ package ferancini.app.despesas.view;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ferancini.app.despesas.R;
+import ferancini.app.despesas.model.Despesa;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +19,8 @@ import ferancini.app.despesas.R;
  * create an instance of this fragment.
  */
 public class OrcamentoFragment extends Fragment {
+    private RecyclerView recyclerV; //pode ser o componente do OrcamentoFragment ou do DespesaFragment
+    private LinearLayoutManager ltmg; // gerenciador de layout
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +66,10 @@ public class OrcamentoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orcamento, container, false);
+        View v = inflater.inflate(R.layout.fragment_orcamento, container, false);
+        recyclerV= v.findViewById(R.id.orcamento_list);
+        DespesaListAdapter adapterDesp = new DespesaListAdapter(v.getContext(), Despesa.getAllDespesas());
+        recyclerV.setAdapter(new DespesaListAdapter(v.getContext(), Despesa.getAllDespesas()));
+        return v;
     }
 }
