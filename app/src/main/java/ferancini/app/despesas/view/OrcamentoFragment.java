550440f5da7396cddf036b56f283539d54b21993
wallace-f-rosa <wferancini@gmail.com>
2020-09-26 11:38:39 -0300

    quando o fragment é resumido reiniciamos a lista
    assumimos que o quando voltamos de uma activity para essa algum item foi adicionado ou alterado.

--- app/src/main/java/ferancini/app/despesas/view/OrcamentoFragment.java
+++ app/src/main/java/ferancini/app/despesas/view/OrcamentoFragment.java
@@ -1,7 +1,9 @@
 package ferancini.app.despesas.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,9 +11,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 import ferancini.app.despesas.R;
 import ferancini.app.despesas.model.Despesa;
+import ferancini.app.despesas.model.Orcamento;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +27,7 @@ import ferancini.app.despesas.model.Despesa;
 public class OrcamentoFragment extends Fragment {
     private RecyclerView recyclerV; //pode ser o componente do OrcamentoFragment ou do DespesaFragment
     private LinearLayoutManager ltmg; // gerenciador de layout
+    private OrcamentoListAdapter adapter;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -63,13 +70,26 @@ public class OrcamentoFragment extends Fragment {
     }
 
     @Override
+    public void onResume() {
+        adapter.notifyDataSetChanged();
+        super.onResume();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_orcamento, container, false);
         recyclerV= v.findViewById(R.id.orcamento_list);
-        DespesaListAdapter adapterDesp = new DespesaListAdapter(v.getContext(), Despesa.getAllDespesas());
-        recyclerV.setAdapter(new DespesaListAdapter(v.getContext(), Despesa.getAllDespesas()));
+
+        if(ltmg == null)
+            ltmg = new LinearLayoutManager(getActivity());
+
+        recyclerV.setLayoutManager(ltmg);
+        if(adapter == null)
+            adapter = new OrcamentoListAdapter(Orcamento.getAllOrcamentos());
+
+        recyclerV.setAdapter(adapter);
         return v;
     }
 }
